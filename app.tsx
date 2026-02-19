bc1f0a2d8f98ffa38b19220a04ebe8a201c5f35e
Lisa Silva <cuddlepolotics@gmail.com>
2026-02-18 19:40:06 -0800

    Add main application structure with routing

---
+++ app.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import Success from "./pages/Success";
+import Download from "./pages/Download";
+import Contact from "./pages/Contact";
+
+function Router() {
+  // make sure to consider if you need authentication for certain routes
+  return (
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/success"} component={Success} />
+      <Route path={"/download"} component={Download} />
+      <Route path={"/contact"} component={Contact} />
+      <Route path={"/404"} component={NotFound} />
+      {/* Final fallback route */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
